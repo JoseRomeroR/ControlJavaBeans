96ef6b1c71c65899b2fb0db78802167b6be3b615
JoseRomero <joseromerodev@gmail.com>
2022-11-23 11:44:51 +0100

    First Upload

---
+++ src/controllers/miControl.java
@@ -0,0 +1,37 @@
+package Controllers;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Region;
+
+public class miControl extends Region {
+
+    private ObjectProperty<EventHandler<MouseEvent>> OnAction = new SimpleObjectProperty<EventHandler<MouseEvent>>();
+
+    public miControl() {
+        super();
+        // marca la zona donde hacer click
+        setStyle("-fx-border-color:red;");
+        setMaxSize(100, 100);
+        this.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                onActionProperty().get().handle(event);
+            }
+        });
+    }
+
+    public final ObjectProperty<EventHandler<MouseEvent>> onActionProperty() {
+        return OnAction;
+    }
+
+    public final void setOnAction(EventHandler<MouseEvent> handler) {
+        OnAction.set(handler);
+    }
+
+    public final EventHandler<MouseEvent> getOnAction() {
+        return OnAction.get();
+    }
+}
